6238fe3dc17962c85cd6be23025be227fcc27593
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-13 23:45:34 -0600

    Use PIL instead CV2

--- oil_spill_segmentation.py
+++ oil_spill_segmentation.py
@@ -1,7 +1,8 @@
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-import cv2
+#import cv2
+from PIL import Image
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
