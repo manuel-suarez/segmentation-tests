cf3971492fa55edb2e484ff15a9e43a63a20221d
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-13 22:08:37 -0600

    Data directories

--- oil_spill_segmentation.py
+++ oil_spill_segmentation.py
@@ -6,3 +6,13 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Data directories
+DATA_DIR = './oil_spill_data/'
+x_train_dir = os.path.join(DATA_DIR, 'train', 'images')
+y_train_dir = os.path.join(DATA_DIR, 'train', 'labels_1D')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val', 'images')
+y_valid_dir = os.path.join(DATA_DIR, 'val', 'labels_1D')
+
+x_test_dir = os.path.join(DATA_DIR, 'test', 'images')
+y_test_dir = os.path.join(DATA_DIR, 'test', 'labels_1D')
