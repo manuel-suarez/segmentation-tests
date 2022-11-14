571f9b0bbc765ea20ae537baa89496e32dd74987
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-13 21:46:53 -0600

    Data dir

--- binary_segmentation.py
+++ binary_segmentation.py
@@ -13,3 +13,12 @@ if not os.path.exists(DATA_DIR):
     print('Loading data...')
     os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
     print('Done!')
+
+x_train_dir = os.path.join(DATA_DIR, 'train')
+y_train_dir = os.path.join(DATA_DIR, 'trainannot')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val')
+y_valid_dir = os.path.join(DATA_DIR, 'valannot')
+
+x_test_dir = os.path.join(DATA_DIR, 'test')
+y_test_dir = os.path.join(DATA_DIR, 'testannot')
