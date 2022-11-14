e6a2064414a28339c0209c9d595dc1e4a0373398
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-13 21:46:39 -0600

    Data dir

--- binary_segmentation.py
+++ binary_segmentation.py
@@ -5,3 +5,11 @@ import cv2
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
+
+DATA_DIR = './data/CamVid/'
+
+# load repo with data if it is not exists
+if not os.path.exists(DATA_DIR):
+    print('Loading data...')
+    os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
+    print('Done!')
