313b8b5ca5f294bce742ae4d8470107dff48a6ba
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-13 22:11:59 -0600

    Result visualization

--- oil_spill_segmentation.py
+++ oil_spill_segmentation.py
@@ -379,3 +379,18 @@ scores = model.evaluate(test_dataloader)
 print("Loss: {:.5}".format(scores[0]))
 for metric, value in zip(metrics, scores[1:]):
     print("mean {}: {:.5}".format(metric.__name__, value))
+
+# Result visualization
+n = 5
+ids = np.random.choice(np.arange(len(test_dataset)), size=n)
+
+for i in ids:
+    image, gt_mask = test_dataset[i]
+    image = np.expand_dims(image, axis=0)
+    pr_mask = model.predict(image)
+
+    visualize(
+        image=denormalize(image.squeeze()),
+        gt_mask=gt_mask.squeeze(),
+        pr_mask=pr_mask.squeeze(),
+    )
