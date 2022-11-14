3cbdd4951e0fdaf46eac6815fc300e56bd392d6e
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-13 22:01:44 -0600

    Data directories

--- multiclass_segmentation.py
+++ multiclass_segmentation.py
@@ -5,3 +5,22 @@ import cv2
 import keras
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Data directories
+DATA_DIR = './data/CamVid/'
+
+# load repo with data if it is not exists
+if not os.path.exists(DATA_DIR):
+    print('Loading data...')
+    os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
+    print('Done!')
+
+x_train_dir = os.path.join(DATA_DIR, 'train')
+y_train_dir = os.path.join(DATA_DIR, 'trainannot')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val')
+y_valid_dir = os.path.join(DATA_DIR, 'valannot')
+
+x_test_dir = os.path.join(DATA_DIR, 'test')
+y_test_dir = os.path.join(DATA_DIR, 'testannot')
+
