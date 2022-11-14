fed4276b7c869d89b6659a7586d75ca087bfd479
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-13 22:13:44 -0600

    Save figures

--- oil_spill_segmentation.py
+++ oil_spill_segmentation.py
@@ -19,7 +19,7 @@ y_test_dir = os.path.join(DATA_DIR, 'test', 'labels_1D')
 
 # Visualize
 # helper function for data visualization
-def visualize(**images):
+def visualize(figname, **images):
     """PLot images in one row."""
     n = len(images)
     plt.figure(figsize=(16, 5))
@@ -29,7 +29,7 @@ def visualize(**images):
         plt.yticks([])
         plt.title(' '.join(name.split('_')).title())
         plt.imshow(image)
-    plt.show()
+    plt.savefig(figname)
 
 
 # helper function for data visualization
@@ -160,6 +160,7 @@ dataset = Dataset(x_train_dir, y_train_dir, classes=['sea_surface', 'oil_spill',
 
 image, mask = dataset[5] # get some sample
 visualize(
+    'oil_spill_figure1.png',
     image=image,
     sea_surface=mask[..., 0].squeeze(),
     oil_spill=mask[..., 1].squeeze(),
@@ -250,6 +251,7 @@ dataset = Dataset(x_train_dir, y_train_dir, classes=['sea_surface', 'oil_spill',
 
 image, mask = dataset[12] # get some sample
 visualize(
+    'oil_spill_figure2.png',
     image=image,
     sea_surface=mask[..., 0].squeeze(),
     oil_spill=mask[..., 1].squeeze(),
@@ -358,7 +360,7 @@ plt.title('Model loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
+plt.savefig('oil_spill_training.png')
 
 # Model evaluation
 test_dataset = Dataset(
@@ -390,6 +392,7 @@ for i in ids:
     pr_mask = model.predict(image)
 
     visualize(
+        f"oil_spill_result{i}.png",
         image=denormalize(image.squeeze()),
         gt_mask=gt_mask.squeeze(),
         pr_mask=pr_mask.squeeze(),
